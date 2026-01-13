a303097f4adeb7acc140d42ec2cb58007a4a2b6d
Josselin Perez <pcjosselin12@gmail.com>
2026-01-12 22:40:38 -0600

    Configurar Flask correctamente para Render

--- app.py
+++ app.py
@@ -218,6 +218,7 @@ def logout():
 
 # ------------------- RUN -------------------
 
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
